a43e0775cb5dc95d759300b773d10f2e9c26e95a
zuojxin <zuojxin@hotmail.com>
2020-08-04 10:27:52 +0800

    pingpong

--- user/sleep.c
+++ user/sleep.c
@@ -1,15 +1,14 @@
+  
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        fprintf(2, "usage: sleep seconds \n");
-        exit();
-    }
-
-    sleep(atoi(argv[1]));
+  if(argc != 2) {
+    fprintf(2, "usage: sleep ticks\n");
     exit();
+  }
+  sleep(atoi(argv[1]));
+  exit();
 }
