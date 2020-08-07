63034ed63ed3e257cf2e4d6c4c4bdf43ab16aef9
zuojxin <zuojxin@hotmail.com>
2020-08-07 14:36:33 +0800

    primes

--- user/primes.c
+++ user/primes.c
@@ -1,17 +1,43 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int 
-is_primes_num(int number){
-    int i, flag = 0;
-    for(i=2; i<=number/2; ++i){
-        if(number%i==0){
-            flag=1;
-            break;
-        }
+
+
+//每一个子进程的任务：
+//1. 输出第一个数字； 
+//2. fork新进程，进行下次过滤
+//3. 将剩下的过滤发送到新的pipe；
+//4. 关闭不用的fd；
+void 
+filter(int input){
+    int num, div;
+    if (read(input, &div, sizeof(num))<=0) exit();
+
+    //1
+    fprintf(2, "prime %d \n", div);
+    int p[2];
+    pipe(p);
+
+    //2
+    if (fork()){
+        close(p[1]);
+        filter(p[0]);
+        return;
     }
-    //flag==0 is primes number
-    return flag;
+
+    close(p[0]);
+
+    //3
+    while(read(input, &num, sizeof(num))){
+        if (num % div != 0)
+            write(p[1], &num, sizeof(num));
+    }
+
+    //4
+    close(p[1]);
+    close(input);
+    wait();
+    exit();
 }
 
 int 
@@ -19,17 +45,11 @@ main(int argc, char *argv[]){
     int p[2];
     pipe(p);
 
-    int number;
-
-    if (fork() == 0){
-        while(read(p[0], &number, 1)!=-1){
-            if(is_primes_num(number))
-                fprintf(2, "prime %d \n", number);
-        }
-    }
-    else{
-        for (int i = 2; i < 35; i++){
-            write(p[1], &i, 1);
+    if(fork()){
+        filter(p[0]);
+    }else{
+        for (int i = 2; i <= 35; i++){
+            write(p[1], &i, sizeof(i));
         }
     }
     exit();
