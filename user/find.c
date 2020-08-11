36fef3bdd8600ed3840d8d599919eca30cb56b37
左久新 <zuojiuxin@zuojiuxindeMBP.lan>
2020-08-11 21:40:00 +0800

    f

--- user/find.c
+++ user/find.c
@@ -11,12 +11,12 @@ fmtname(char *path) {
     char *p;
 
     // Find first character after last slash.
-    for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    for (p=path+strlen(path); p >= path && *p != '/'; p--)
         ;
     p++;
 
     // Return blank-padded name.
-    if(strlen(p) >= DIRSIZ)
+    if (strlen(p) >= DIRSIZ)
         return p;
     memmove(buf, p, strlen(p));
     memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
@@ -24,61 +24,68 @@ fmtname(char *path) {
 }
 
 
-void 
+void
 find(char*path, char*name) {
     int fd;
     struct stat st;
 
-    if((fd = open(path, 0)) < 0){
+    if ((fd = open(path, 0)) < 0) {
         fprintf(2, "find: cannot open %s\n", path);
         return;
     }
 
-    if(fstat(fd, &st) < 0){
+    if (fstat(fd, &st) < 0) {
         fprintf(2, "find: cannot stat %s\n", path);
         close(fd);
         return;
-    } 
+    }
 
     char buf[512], *p;
     struct dirent de;
 
-    if (st.type == T_FILE){
-        if (fmtname(path) == name){
-                printf("%s \n", path);
+    if (st.type == T_FILE) {
+        if (strcmp(fmtname(path), name)) {
+            fprintf(2, "%s \n", path);
         }
         return;
     }
-      if (st.type == T_DIR){     
-            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-                return;
-            }
-            strcpy(buf, path);
-            p = buf+strlen(buf);
-            *p++ = '/';
-            while(read(fd, &de, sizeof(de)) == sizeof(de)){
-                if(de.inum == 0)
-                    continue;
-                memmove(p, de.name, DIRSIZ);
-                p[DIRSIZ] = 0;
-                if (buf == name){
-                    printf("");
-                }else{
-                    find(buf, name);
-                }
-            }
+
+    if (st.type != T_DIR)
+        return;
+
+    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+        return;
+
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+
+        if (de.inum == 0)
+            continue;
+
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+
+        if (strcmp(de.name, '.'))
+            continue;
+
+        if (strcmp(de.name, name))
+            fprintf(2, "%s \n", buf);
+        else
+            find(buf, name);
     }
+
 }
 
 int
 main(int argc, char *argv[]) {
-    if (argc < 2){
+    if (argc < 2) {
         fprintf(2, "not find file name");
         exit();
     }
 
-    char *name = argv[1];
-
-    find(".", name);
+    find(argv[0], argv[1]);
     exit();
 }
