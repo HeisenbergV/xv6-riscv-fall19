f01a30fa21123dcb3e42bd7ac90d573a017fa486
zuojxin <zuojxin@hotmail.com>
2020-08-11 18:45:53 +0800

    f

--- user/find.c
+++ user/find.c
@@ -3,11 +3,30 @@
 #include "user/user.h"
 #include "kernel/fs.h"
 
-void
-find(char *path, char *name) {
-    char buf[512], *p;
+
+
+char*
+fmtname(char *path) {
+    static char buf[DIRSIZ+1];
+    char *p;
+
+    // Find first character after last slash.
+    for(p=path+strlen(path); p >= path && *p != '/'; p--)
+        ;
+    p++;
+
+    // Return blank-padded name.
+    if(strlen(p) >= DIRSIZ)
+        return p;
+    memmove(buf, p, strlen(p));
+    memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+    return buf;
+}
+
+
+void 
+find(char*path, char*name) {
     int fd;
-    struct dirent de;
     struct stat st;
 
     if((fd = open(path, 0)) < 0){
@@ -19,25 +38,47 @@ find(char *path, char *name) {
         fprintf(2, "find: cannot stat %s\n", path);
         close(fd);
         return;
-    }
+    } 
 
-    if(read(fd, &de, sizeof(de) < 0)){
-        return;
-    }
-    
-    if (de.name == name){
-        return;
-    }
+    char buf[512], *p;
+    struct dirent de;
 
-    if (st.type==T_DIR){
-        find(path+name, name);
+    if (st.type == T_FILE){
+        if (fmtname(path) == name){
+                printf("%s \n", path);
+        }
         return;
+    }
+      if (st.type == T_DIR){     
+            if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+                return;
+            }
+            strcpy(buf, path);
+            p = buf+strlen(buf);
+            *p++ = '/';
+            while(read(fd, &de, sizeof(de)) == sizeof(de)){
+                if(de.inum == 0)
+                    continue;
+                memmove(p, de.name, DIRSIZ);
+                p[DIRSIZ] = 0;
+                if (buf == name){
+                    printf("");
+                }else{
+                    find(buf, name);
+                }
+            }
     }
 }
 
 int
 main(int argc, char *argv[]) {
+    if (argc < 2){
+        fprintf(2, "not find file name");
+        exit();
+    }
 
-    find(".");
+    char *name = argv[1];
+
+    find(".", name);
     exit();
 }
